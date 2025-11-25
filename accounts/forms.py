cd811f9ea67ebad23fd34102718485044ae44f5b
alswn <alswn12004@gmail.com>
2025-11-26 00:59:42 +0900

    로그인,가입,로그아웃,판매글 등록, 신고, 프론트 디자인 완료/내상점에서 사용자 이름 누락, 찜 목록 이미지 누락,회원가입시 이름따로닉네임 따로 수정 예정

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,29 @@
-# accounts/forms.py
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UserRegistrationForm(UserCreationForm):
+    nickname = forms.CharField(
+        max_length=30,
+        required=False,
+        label="닉네임",
+        widget=forms.TextInput(attrs={
+            "class": "w-full border border-gray-300 rounded-lg px-3 py-2 text-sm",
+            "placeholder": "닉네임 (선택)",
+        }),
+    )
+
     class Meta(UserCreationForm.Meta):
         model = User
-        # username, password1, password2 + email 필드까지 사용
-        fields = UserCreationForm.Meta.fields + ('email',)
+        fields = UserCreationForm.Meta.fields + ("email",)
+
+    def save(self, commit=True):
+        user = super().save(commit)
+        nickname = self.cleaned_data.get("nickname")
+        if nickname:
+            # Profile은 시그널로 자동 생성되니까 가져와서 닉네임만 세팅
+            user.profile.nickname = nickname
+            user.profile.save()
+        return user
